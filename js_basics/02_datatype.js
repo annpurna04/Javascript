be9eb041a1e8b282f64f65159edcb62ef958d1e4
Annpurna Gupta <65756912+annpurna04@users.noreply.github.com>
2024-12-17 09:25:59 +0000

    Datatype

---
+++ js_basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version 
+
+// alert( 3+2) we are using nodejs, not browser 
+
+let name ="Anu" // string
+let age = 20  //integer, number
+let isLoggedIn= false //boolean
+
+// number => 2 to power 53
+//bigint used in stockmarket, facebook()when number is very big
+//string can be in '' & ""
+//boolean => true/false
+//null  => standalone value (typeis object)
+//undefined => value not assign (typeis undefined)
+//symbol => unique in component
+
+
+//object 
+
+//console.log(typeof undefined) undefined
+//console.log(typeof null) object
