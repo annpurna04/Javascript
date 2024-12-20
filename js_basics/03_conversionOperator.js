575a4d673de3f9eba1072f9369bb0dad6dd96b32
annpura04 <annpurnagupta040401@gmail.com>
2024-12-20 16:25:14 +0530

    conversionofDatatype

---
+++ js_basics/03_conversionOperator.js
@@ -0,0 +1,25 @@
+let score = "3abc"
+console.log(score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = "anu"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 => true; o=> false
+//"" => false
+//"anu" =>true
+
+// Converting number to string
+ let num=7
+ let s=String(num)
+ console.log(s)
+ console.log(typeof s);
+ 
